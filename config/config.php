45963622ab6bab07e22ba2ace3a544590f728424
ola <olanochristopher@gmail.com>
2019-06-28 01:17:01 +0800

    db updated

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
 	//displays the date
 	//echo "<span style='color:red;font-weight:bold;'>Date: </span>". date('F j, Y g:i:a  ');
 
-	$con = mysqli_connect("localhost","root","","social");
+	$con = mysqli_connect("ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","jvtvm907r10e1vtj","xra5kndfb6ap4g90","bejhu7hym9q9z0f4");
 
 	/*return an error*/
 	if(mysqli_connect_errno()){
